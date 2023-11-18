fc46cd87ee80dea0c89961745ef2feed8d5fb042
devjoseph1992 <devjosephflorina@gmail.com>
2023-11-18 08:14:36 +0800

    adding modal and providers for client/server

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "../ui/model";
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      forms
+    </Modal>
+  );
+};
